6689f3d63893e15f96623180238ecf8b295b94a5
Maxim-Koryagin <theKosmokos@icloud.com>
2022-11-16 12:33:52 +0300

    HW 2.1 done, firs task

--- Navigation/TabBarModule /TabBarController.swift
+++ Navigation/TabBarModule /TabBarController.swift
@@ -12,6 +12,7 @@ final class TabBarController: UITabBarController {
     
     var firstTabNavidationController: UINavigationController!
     var secondTabNavigationController: UINavigationController!
+    var thirdTabNavigationController: UINavigationController!
     
     // MARK: Life cycle
     
@@ -28,15 +29,19 @@ final class TabBarController: UITabBarController {
         loginViewController.loginDelegate = MyLoginFactory().makeLoginInspector()
         
         firstTabNavidationController = UINavigationController.init(rootViewController: FeedViewController())
-        secondTabNavigationController = UINavigationController.init(rootViewController: loginViewController)
-
-        viewControllers = [firstTabNavidationController, secondTabNavigationController]
+        secondTabNavigationController = UINavigationController.init(rootViewController: AudioViewController())
+        thirdTabNavigationController = UINavigationController.init(rootViewController: loginViewController)
+        
+        viewControllers = [firstTabNavidationController, secondTabNavigationController, thirdTabNavigationController]
         
         let item1 = UITabBarItem(title: "Feed", image: UIImage(systemName: "square.grid.2x2"), tag: 0)
-        let item2 = UITabBarItem(title: "Profile", image:  UIImage(systemName: "person"), tag: 1)
+        let item2 = UITabBarItem(title: "Audio", image: UIImage(systemName: "music.note"), tag: 1)
+        let item3 = UITabBarItem(title: "Profile", image:  UIImage(systemName: "person"), tag: 2)
+       
         
         firstTabNavidationController.tabBarItem = item1
         secondTabNavigationController.tabBarItem = item2
+        thirdTabNavigationController.tabBarItem = item3
         
         UITabBar.appearance().tintColor = UIColor(red: 0/255.0, green: 146/255.0, blue: 248/255.0, alpha: 1)
         UITabBar.appearance().backgroundColor = .white
